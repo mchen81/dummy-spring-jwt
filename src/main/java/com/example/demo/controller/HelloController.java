e1336bb8a8c7598bad85f684718ddd5cc39b6f98
Jerry <mchen81@dons.usfca.edu>
2021-07-04 20:30:07 +0800

    define logging location

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping
     public ResponseEntity<String> sayHello() {
+        logger.info("Someone says hello");
         return ResponseEntity.ok("Hello, every one can view this page.");
     }
 
     @PostMapping()
     public ResponseEntity<String> makeAPost() {
+        logger.info("Someone makes a post");
         return ResponseEntity.ok("You made a post, but nothing changed.");
     }
 
